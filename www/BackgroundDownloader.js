2c738fbdfe6f51f136b1c562d27b486613fa97b6
yuviyuvan13 <43641037+yuviyuvan13@users.noreply.github.com>
2018-09-27 19:39:39 +0530

    Update BackgroundDownloader.js

--- www/BackgroundDownloader.js
+++ www/BackgroundDownloader.js
@@ -35,8 +35,8 @@ var BackgroundDownloader = function() {
  * @param {string} uri The location of the resource.
  * @param {File} resultFile The file that the response will be written to.
  */
-BackgroundDownloader.prototype.createDownload = function(uri, resultFile) {
-    return new DownloadOperation(uri, resultFile);
+BackgroundDownloader.prototype.createDownload = function(uri, resultFile,notificationTitle) {
+    return new DownloadOperation(uri, resultFile,notificationTitle);
 };
 
 module.exports = BackgroundDownloader;
